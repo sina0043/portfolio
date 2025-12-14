84ee898c9fe44959cfe9a7e3387cc54aefa2c6c8
Sina Naseri <sinafx2000@gmail.com>
2025-12-14 11:46:23 +0330

    deploy React SPA to GitHub Pages

--- deploy.ps1
+++ deploy.ps1
@@ -1,27 +1,21 @@
 $projectName = "portfolio"
 $branchName = "main"
+$repoName = "portfolio"
+
+$env:VITE_REPO_NAME = $repoName
 
 if (Test-Path "docs") {
     Remove-Item -Recurse -Force "docs"
 }
 
-ng build --configuration=production --base-href="/$projectName/"
+npm run build
 
 New-Item -ItemType Directory -Path "docs"
 
-$csrFile = "dist\$projectName\browser\index.csr.html"
-$indexFile = "docs\index.html"
+Copy-Item -Recurse -Force "dist/*" "docs/"
 
-if (Test-Path $csrFile) {
-    Copy-Item $csrFile $indexFile
-} else {
-    exit
-}
+Remove-Item Env:VITE_REPO_NAME
 
-Copy-Item $indexFile "docs/404.html"
-
-Copy-Item -Recurse -Force "dist\$projectName\browser\*" "docs/"
-
-# git add .
-# git commit -m "deploy Angular SPA to GitHub Pages"
-# git push -u origin $branchName
+git add .
+git commit -m "deploy React SPA to GitHub Pages"
+git push -u origin $branchName
